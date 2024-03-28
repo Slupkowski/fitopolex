a72ed7420e5ef384ca0575033b90f6e3f208770b
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-03-28 21:08:23 +0100

    Init Header

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { Button } from '@mui/material'
+import { Header } from './components/Header'
 
-export default App = () => {
-
+const App = () => {
   const apiCall = () => {
     axios.get('http://localhost:3000').then((data) => {
       //this console.log will be in our frontend console
@@ -11,11 +11,13 @@ export default App = () => {
   }
 
   return (
-    <div className="App">
+    <div>
+        <Header/>
         <p className="text-3xl font-bold underline"> Grypsuje...</p>
         <button onClick={apiCall}>Make API Call</button>
         <Button>Tomek</Button>
-
+        <Button variant="contained" size="large" color="error">ZARA TO ROZPIERDOLE</Button>
     </div>
   );
 }
+export default  App;
