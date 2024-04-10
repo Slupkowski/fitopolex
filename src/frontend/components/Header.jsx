f43b7cfa9e285c851e5776839c7f33f85f06fe37
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-04-10 22:00:08 +0200

    Remove tailwindcss

--- src/frontend/components/Header.jsx
+++ src/frontend/components/Header.jsx
@@ -1,4 +1,11 @@
-import { Box, Button, AppBar, Toolbar, IconButton } from "@mui/material";
+import {
+  Box,
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton,
+  ButtonGroup,
+} from "@mui/material";
 import {
   AccountCircleSharp,
   Menu,
@@ -7,54 +14,53 @@ import {
 
 export function Header() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <Menu />
-          </IconButton>
-          <Box
-            sx={{
-              display: "flex",
-              flexGrow: 1,
-              justifyContent: "Center",
-              gap: 2,
-              width: 1,
-            }}
-          >
+    <AppBar>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <Menu />
+        </IconButton>
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            justifyContent: "Center",
+            gap: 2,
+          }}
+        >
+          <ButtonGroup>
             <Button variant="contained" color="warning">
               O nas
             </Button>
             <Button>Cele</Button>
             <Button>Plany Treningowe</Button>
             <Button>Kontakt</Button>
-          </Box>
-          <IconButton
-            size="large"
-            edge="start"
-            color="red"
-            aria-label="AccountCircleSharpIcon"
-            sx={{ mr: 2 }}
-          >
-            <AccountCircleSharp />
-          </IconButton>
-          <IconButton
-            size="large"
-            edge="start"
-            color="red"
-            aria-label="AccountCircleSharpIcon"
-            sx={{ mr: 2 }}
-          >
-            <ShoppingCartSharp />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          </ButtonGroup>
+        </Box>
+        <IconButton
+          size="large"
+          edge="start"
+          color="red"
+          aria-label="AccountCircleSharpIcon"
+          sx={{ mr: 2 }}
+        >
+          <AccountCircleSharp />
+        </IconButton>
+        <IconButton
+          size="large"
+          edge="start"
+          color="red"
+          aria-label="AccountCircleSharpIcon"
+          sx={{ mr: 2 }}
+        >
+          <ShoppingCartSharp />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
