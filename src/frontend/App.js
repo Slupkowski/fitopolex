f3850a5db26a6932304b37403b41884c8b3597bc
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-05-08 21:38:43 +0200

    added routing

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,23 +1,33 @@
-import axios from 'axios';
-import { Button } from '@mui/material'
-import { Header } from './components/Header'
+import axios from "axios";
+import { Route, Routes } from "react-router";
+import { Header } from "./components/Header";
+import { Contact } from "./pages/Contact";
+import { Plans } from "./pages/Plans";
+import { Welcome } from "./pages/Welcome";
 
 const App = () => {
   const apiCall = () => {
-    axios.get('http://localhost:3000').then((data) => {
+    axios.get("http://localhost:3000").then((data) => {
       //this console.log will be in our frontend console
-      console.log(data)
-    })
-  }
+      console.log(data);
+    });
+  };
 
   return (
     <div>
-        <Header/>
-        <p className="text-3xl font-bold underline"> Grypsuje...</p>
-        <button onClick={apiCall}>Make API Call</button>
-        <Button>Tomek</Button>
-        <Button variant="contained" size="large" color="error">ZARA TO ROZPIERDOLE</Button>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      {/* <p className="text-3xl font-bold underline"> Grypsuje...</p>
+      <button onClick={apiCall}>Make API Call</button>
+      <Button>Tomek</Button>
+      <Button variant="contained" size="large" color="error">
+        ZARA TO ROZPIERDOLE
+      </Button> */}
     </div>
   );
-}
-export default  App;
+};
+export default App;
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,15 +1,18 @@
 import { StrictMode } from "react";
-import App from './App';
-import { createRoot } from 'react-dom/client';
+import App from "./App";
+import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from './theme/index';
+import { BrowserRouter as Router } from "react-router-dom";
+import theme from "./theme/index";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
   </StrictMode>
 );
