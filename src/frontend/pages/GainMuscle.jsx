213415b230fc4af20f7a8f2f427260f809f1c30e
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-05-29 09:32:43 +0200

    filled websites with basic text

--- src/frontend/pages/GainMuscle.jsx
+++ src/frontend/pages/GainMuscle.jsx
@@ -1,5 +1,51 @@
 import { Header } from "../components/Header";
+import { Typography, Box } from "@mui/material";
 
 export const GainMuscle = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Box sx={{ marginTop: "110px" }} />
+      <Box
+        sx={{
+          width: "80%",
+          height: "200px",
+          margin: "0 auto",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus
+          massa metus, vitae interdum dui tincidunt at. Nunc vitae libero non
+          est dictum malesuada vel ac dolor. Mauris mauris massa, blandit sit
+          amet metus eget, pulvinar elementum mi. Sed id massa non lorem feugiat
+          tempus. Donec volutpat sed mauris quis maximus. Duis sodales porta
+          pulvinar. Etiam ut luctus urna. Nulla ultrices egestas dolor sit amet
+          faucibus.
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: "80%",
+          height: "200px",
+          margin: "0 auto",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus
+          massa metus, vitae interdum dui tincidunt at. Nunc vitae libero non
+          est dictum malesuada vel ac dolor. Mauris mauris massa, blandit sit
+          amet metus eget, pulvinar elementum mi. Sed id massa non lorem feugiat
+          tempus. Donec volutpat sed mauris quis maximus. Duis sodales porta
+          pulvinar. Etiam ut luctus urna. Nulla ultrices egestas dolor sit amet
+          faucibus.
+        </Typography>
+      </Box>
+    </div>
+  );
 };
