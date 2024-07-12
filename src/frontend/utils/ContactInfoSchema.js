abdc246501b482fad7acd1530eb3960acd651d60
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-07-12 16:34:03 +0200

    added schema for contact info

--- src/frontend/utils/ContactInfoSchema.js
+++ src/frontend/utils/ContactInfoSchema.js
@@ -3,4 +3,6 @@ import { z } from "zod";
 export const contactInfoSchema = z.object({
   firstName: z.string().max(30),
   mailName: z.string().max(30),
+  agreeContact: z.boolean(),
+  agreeSafety: z.boolean(),
 });
