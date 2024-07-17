4616be03f25883f0a6e2415b15e2bc70ff3ed350
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-07-17 21:33:06 +0200

    Finished schema for TrainingInfo form

--- src/frontend/utils/TrainingInfoSchema.js
+++ src/frontend/utils/TrainingInfoSchema.js
@@ -1,9 +1,45 @@
 import { z } from "zod";
 
 export const trainingInfoSchema = z.object({
-  timeInfo: z.number().gte(0).lte(2),
-  equipmentInfo: z.number().gte(0).lte(2),
-  goalInfo: z.number().gte(0).lte(2),
-  extraGoalInfo: z.number().gte(0).lte(2),
+  timeInfo: z
+    .number()
+    .gte(0, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    })
+    .lte(2, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    }),
+  equipmentInfo: z
+    .number()
+    .gte(0, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    })
+    .lte(2, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    }),
+  goalInfo: z
+    .number()
+    .gte(0, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    })
+    .lte(2, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    }),
+  extraGoalInfo: z
+    .number()
+    .gte(0, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    })
+    .lte(2, {
+      message:
+        "nienawidze tego pierrdolonego gówna błagam niech już w końcu zdechne",
+    }),
   houseEquipment: z.boolean().array().length(4).optional(),
 });
