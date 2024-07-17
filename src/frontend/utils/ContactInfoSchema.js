40a66234b4f0d977a2eb4cc2674247bbcfe91650
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-07-17 21:32:45 +0200

    Finished schema for ContactInfo form

--- src/frontend/utils/ContactInfoSchema.js
+++ src/frontend/utils/ContactInfoSchema.js
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 export const contactInfoSchema = z.object({
-  firstName: z.string().max(30),
-  mailName: z.string().max(30),
+  firstName: z
+    .string()
+    .max(30, { message: "Imię nie powinno być dłuższe niż 30 znaków" })
+    .min(3, { message: "Imię nie powinno być krótsze niż 3 znaki" }),
+  mailName: z
+    .string({ message: "Wprowadź email" })
+    .email({ message: "Wprowadzony email nie jest poprawny" }),
   agreeContact: z.boolean(),
   agreeSafety: z.boolean(),
 });
