af7200711fe47d160b3afe7c6be627d2be717af4
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-07-17 21:30:49 +0200

    Finally finished controlled radio

--- src/frontend/components/controlled/ControlledRadio.jsx
+++ src/frontend/components/controlled/ControlledRadio.jsx
@@ -1,4 +1,7 @@
 import { Radio } from "../Radio";
+import { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { WarningAmberOutlined } from "@mui/icons-material";
 export const ControlledRadio = ({
   inputName,
   options,
@@ -7,7 +10,11 @@ export const ControlledRadio = ({
   name,
   control,
   setValue,
+  errorMessage,
 }) => {
+  useEffect(() => {
+    setValue(name, 99);
+  }, []);
   const handleClick = (index) => {
     setValue(name, index);
   };
@@ -20,7 +27,17 @@ export const ControlledRadio = ({
         onChange={handleClick}
         tooltipText={tooltipText}
         isRequired={isRequired}
+        errorMessage={errorMessage}
       />
+      {/* {errorMessage && ()} */}
+      {errorMessage ? (
+        <Box sx={{ display: "flex", gap: "5px" }}>
+          <WarningAmberOutlined sx={{ color: "red" }} />
+          <Typography sx={{ color: "red", marginTop: "3px" }}>
+            {errorMessage}
+          </Typography>
+        </Box>
+      ) : null}
     </>
   );
 };
