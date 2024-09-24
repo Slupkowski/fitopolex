eac6df70e5fa8e08f709ce48e3251230c932313d
Slupkowski <gdx148252@student.wsb.gda.pl>
2024-09-24 19:29:47 +0200

    working on functionality logic

--- src/backend/controllers/PdfCreator.js
+++ src/backend/controllers/PdfCreator.js
@@ -1,35 +1,49 @@
 const fs = require("fs");
-const PDFDocument = require("pdfkit-table");
+const { default: PDFDocumentWithTables } = require("pdfkit-table");
 
-let doc = new PDFDocument({ margin: 30, size: "A4" });
-// save document
+const doc = new PDFDocumentWithTables({ margin: 30, size: "A4" });
+
 doc.pipe(
-  fs.createWriteStream("../../documents/fitopolex/src/documents/dupa.pdf")
+  fs.createWriteStream("../../documents/fitopolex/src/documents/ratunku1.pdf")
 );
+doc.text("Plan Treningowy");
 
 (async function () {
-  // table
   const table = {
-    title: "Title",
-    subtitle: "Subtitle",
-    headers: ["Country", "Conversion rate", "Trend"],
+    title: "Plan A",
+    headers: ["Ćwiczenie", "ilość powtórzeń"],
     rows: [
-      ["Switzerland", "12%", "+1.12%"],
-      ["France", "67%", "-0.98%"],
-      ["England", "33%", "+4.44%"],
+      ["Przysiad", "5x5"],
+      ["Wyciskanie", "5x5"],
+      ["Wiosłowanie", "5x5"],
     ],
   };
-  // A4 595.28 x 841.89 (portrait) (about width sizes)
-  // width
   await doc.table(table, {
     width: 300,
   });
-  // or columnsSize
   await doc.table(table, {
     columnsSize: [200, 100, 100],
   });
-  // done!
-  doc.end();
 })();
-// function createPdf(doc) {}
-// module.exports = { createPdf };
+(async function () {
+  const table = {
+    title: "Plan B",
+    headers: ["Ćwiczenie", "ilość powtórzeń"],
+    rows: [
+      ["Martwy Ciąg", "5x5"],
+      ["Wyciskanie Żołnierskie", "5x5"],
+      ["Podciąganie", "5x5"],
+    ],
+  };
+  await doc.table(table, {
+    width: 300,
+  });
+  await doc.table(table, {
+    columnsSize: [200, 100, 100],
+  });
+})();
+doc.font(
+  "../../documents/fitopolex/src/fonts/RobotoSlab-VariableFont_wght.ttf"
+);
+doc.text("Test PDF bez tabeli z ąśźżćł.");
+doc.end();
